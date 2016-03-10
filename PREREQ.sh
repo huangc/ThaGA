f809db9d90011d10042fdfa02358685bc8b5cfba
Chun-Yuan Huang <huangc@users.noreply.github.com>
2016-03-10 18:29:01 -0500

    Update PREREQ.sh

--- PREREQ.sh
+++ PREREQ.sh
@@ -4,12 +4,10 @@ source ./0SOURCE
 ## Prerequisite data:
 # The soapdenovo2-assembled contigs of genomic samples.
 # The reference sequence.
-# The paired-TALEN (pTALEN) repeat variable diresidue (RVD).
 
 ## Prerequisite software:
 # NCBI BLAST+
 # blat/35 (The BLAST-Like Alignment Tool)
-# TALE-NT2 Target Finder 
 
 ##--------------------------------------------
 # Setup sub-directory for workflow
@@ -23,7 +21,7 @@ mkdir -p ${WORK_DIR}/run
 mkdir -p ${WORK_DIR}/scratch
 
 ## Prepare for the SOAPdenovo2-assembled sample contigs.
-# Note: the contigs have been assembled previously, and we are just retrieving those contig files.
+# Note: the contigs have been assembled previously using TRegGA workflow, and we are just linking those contig files here.
 cd ${prereq_DIR}
 for i in ${SAMPLE}
 do
@@ -31,15 +29,7 @@ ln -s ${denovo_DIR}/${i}/${i}-SOAP/${i}-soap.contig .
 makeblastdb -in ${i}-soap.contig -dbtype nucl -out DB_${i}_contig -parse_seqids
 done
 
-## Prepare the pTALEN RVD sequences for use in the TALE-NT2 target finder
-# Generate pTALEN RVD as follows:
-# echo '"rvdseq1" "rvdseq2"' > pTALEN_Name
-
-echo '"HD NG NG HD HD NG NG HD HD NG NI NN HD NI HD NG NI NG NI NG NI NI" "NN HD NG NG NN NI NI NN NN HD NG NG NN NI NG NN NI NN HD NG NG NI"' > pTALEN_Os11N3
-
-echo '"NG NI NN NI NG NI NG NN HD NI NG HD NG HD HD HD NG" "NI NG NI NG NI NN NG NG NN NN NI NN NI HD HD HD NG HD HD NI HD NG NG"' > pTALEN_Os8N3
-
-## Retrieve and index the reference genome
+# Retrieve and index the reference genome
 echo "
 #!/bin/bash
 #PBS -m abe
